484b638d9cfe1d7203832cfe276c57b9e6e5a124
yunhubuxi_Y400 <296023290@qq.com>
2020-02-17 09:35:30 +0800

    Update Basic.java

--- concurrency/src/main/java/thread/local/Basic.java
+++ concurrency/src/main/java/thread/local/Basic.java
@@ -3,12 +3,15 @@ package thread.local;
 public class Basic {
     public static ThreadLocal<Long> x = ThreadLocal.withInitial(() -> {
         System.out.println("initialValue...");
-        return 100L;
+        return Thread.currentThread().getId();
     });
 
 
     public static void main(String[] args) {
-        System.out.println(x.get());
-        System.out.println(x.get());
+        new Thread(() -> System.out.println(x.get())).start();
+        new Thread(() -> System.out.println(x.get())).start();
+        new Thread(() -> System.out.println(x.get())).start();
+        x.set(10000L);
+        System.out.printf("main => %d%n", x.get());
     }
 }
