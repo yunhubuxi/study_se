bdeca6343a0081aef1672c8bb47c40612e4479dc
yunhubuxi <296023290@qq.com>
2020-06-17 11:13:19 +0800

    。。。

---
+++ concurrency/src/main/java/juc/CountDownLatchExample1.java
@@ -0,0 +1,40 @@
+package juc;
+
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchExample1 {
+
+    private final static int threadCount = 200;
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            exec.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (Exception e) {
+                    System.err.println(e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        System.out.println("finish");
+        exec.shutdown();
+    }
+
+    private static void test(int threadNum) throws Exception {
+        Thread.sleep(100);
+        System.out.println(threadNum);
+        Thread.sleep(100);
+    }
+}
---
+++ concurrency/src/main/java/juc/CyclicBarrierExample2.java
@@ -0,0 +1,41 @@
+package juc;
+
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class CyclicBarrierExample2 {
+
+    private static CyclicBarrier barrier = new CyclicBarrier(5);
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executor.execute(() -> {
+                try {
+                    race(threadNum);
+                } catch (Exception e) {
+                    System.err.println(e);
+                }
+            });
+        }
+        executor.shutdown();
+    }
+
+    private static void race(int threadNum) throws Exception {
+        Thread.sleep(1000);
+        System.out.println(threadNum + "is ready");
+        try {
+            barrier.await(2000, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        System.out.println(threadNum + "is continue");
+    }
+}
