57a29a250a033e69c010650ecace497934f717d2
yunhubuxi <296023290@qq.com>
2020-06-30 20:32:46 +0800

    。。。

--- utils/src/main/java/jol/JavaObjectLayout.java
+++ utils/src/main/java/jol/JavaObjectLayout.java
@@ -6,6 +6,7 @@ public class JavaObjectLayout {
     public static void main(String[] args) {
         System.out.println("------------Object---------------");
         Object o = new Object();
+        System.out.println(o.hashCode());
         String s = ClassLayout.parseInstance(o).toPrintable();
         System.out.println(s);
     }
