f9227f525dfce0d3573c36542ccf3378858f94db
yunhubuxi <296023290@qq.com>
2020-08-06 15:23:02 +0800

    算法题

---
+++ algorithm/src/main/java/array/RemoveDuplicate.java
@@ -0,0 +1,35 @@
+package array;
+
+/**
+ * 删除排序数组中的重复项
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ */
+public class RemoveDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 4, 5, 5};
+        int len = removeDuplicates(nums);
+
+        for (int i = 0; i < len; i++) {
+            System.out.println(nums[i]);
+        }
+
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i - 1]) {
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }
+        }
+        return nums.length;
+    }
+
+
+}
