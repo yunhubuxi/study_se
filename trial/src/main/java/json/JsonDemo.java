c741a61093dc1e7985d565ff7cce50226e25efa0
yunhubuxi_Y400 <296023290@qq.com>
2020-09-05 16:29:59 +0800

    测试json字符串

---
+++ trial/src/main/java/json/JsonDemo.java
@@ -0,0 +1,14 @@
+package json;
+
+public class JsonDemo {
+    public static void main(String[] args) {
+        String aa = "\"";
+        String bb = "\\";
+        String cc = "\\\\";
+        String dd = "\"\\\"";
+        System.out.println(aa);
+        System.out.println(bb);
+        System.out.println(cc);
+        System.out.println(dd);
+    }
+}
