0e3c1493ddd7e1d038d82eb14af3bd5c92d328fa
yunhubuxi <296023290@qq.com>
2020-09-21 17:08:57 +0800

    测试CompletableFuture

---
+++ concurrency/src/main/java/completable/CompletableFutureTest3.java
@@ -0,0 +1,73 @@
+package completable;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableFutureTest3 {
+    public static void main(String[] args) throws Exception {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        CompletableFuture<String> resultA = null;
+        CompletableFuture<String> resultB = null;
+        for (String s : list) {
+            switch (s) {
+                case "a":
+                    resultA = getForecastList1();
+                    break;
+                case "b":
+                    resultB = getForecastList2();
+                    break;
+                    default:
+                        System.out.println("fff");
+            }
+        }
+        List<String> result = new ArrayList<>();
+        System.out.println("============开始等待===============");
+        result.add(resultA.get());
+        System.out.println("111111111111");
+        result.add(resultB.get());
+        System.out.println("222222222222222222");
+        System.out.println(result);
+    }
+
+    private static String test1() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(Thread.currentThread().getName() +"==============test1");
+        return "aa";
+    }
+
+    private static CompletableFuture<String> getForecastList1() {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return test1();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return null;
+            }
+        });
+    }
+
+    private static String test2() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(15);
+        System.out.println(Thread.currentThread().getName() + "==============test2");
+        return "bb";
+    }
+
+    private static CompletableFuture<String> getForecastList2() {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return test2();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return null;
+            }
+        });
+    }
+}
--- trial/src/main/java/Test.java
+++ trial/src/main/java/Test.java
@@ -1,28 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.LocalDateTime;
 
 public class Test {
     public static void main(String[] args) {
-        List<User> list = new ArrayList<>();
-        User a = new User();
-        a.age = 10;
-        User b = new User();
-        b.age = 20;
-        User c = new User();
-        c.age = 10;
-        User d = new User();
-        d.age = 20;
-        list.add(a);
-        list.add(b);
-        list.add(c);
-        list.add(d);
-        list.forEach(user -> System.out.println("list" + user.age + ">" + user.name));
-        List<User> list1 = list.stream().filter(user -> user.age == 10).collect(Collectors.toList());
-        list1.forEach(user -> System.out.println("list1" +user.age + ">" + user.name));
-        list.stream().filter(e ->e.age == 10).forEach(user -> user.name = "fsd");
-        list.forEach(user -> System.out.println("list" +user.age + ">" + user.name));
-        list1.forEach(user -> System.out.println("list1" +user.age + ">" + user.name));
+        System.out.println(LocalDateTime.now().plusDays(1));
+        System.out.println(LocalDateTime.now().plusDays(-1));
 
     }
 
