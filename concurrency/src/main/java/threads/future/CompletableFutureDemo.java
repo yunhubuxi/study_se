5442fbd580fb8670f1fc2b876f5393f6fab09f3e
yunhubuxi <296023290@qq.com>
2020-12-08 16:08:34 +0800

    添加注释

--- concurrency/src/main/java/threads/future/CompletableFutureDemo.java
+++ concurrency/src/main/java/threads/future/CompletableFutureDemo.java
@@ -4,6 +4,16 @@ import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Future和Callable需要借助ExecutorService才能使用
+ * ExecutorService实际上是借助了RunnableFuture的实现类FutureTask来完成的
+ * Future、Callable、Runnable可以直接借助FutureTask，脱离ExecutorService，但是不能脱离Thread而运行
+ * FutureTask可以把Runnable转换成Callable来执行
+ * FutureTask实际上还是一个Task，只是持有了线程的句柄，自身并不能执行，还需要穿上Thread的衣服
+ * CompletableFuture实现了Future和CompletionStage接口，具有了Future持有线程句柄的功能，同时具有编排线程的功能
+ * 最重要的是CompletableFuture自身具有线程池的功能，所以不仅仅是Task，也不仅仅是持有线程的句柄，自己就可以执行Task
+ * CompletableFuture是整合了Future和ExecutorService，并且添加了编排线程的功能
+ */
 public class CompletableFutureDemo {
     public static void main(String[] args) throws Exception {
         whenComplete();
