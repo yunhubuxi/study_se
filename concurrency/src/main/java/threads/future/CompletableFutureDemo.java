ff08dc6a1c9083289f0286868a1dd171abf308db
yunhubuxi <296023290@qq.com>
2020-12-08 15:44:28 +0800

    CompletableFuture

---
+++ concurrency/src/main/java/threads/future/CompletableFutureDemo.java
@@ -0,0 +1,66 @@
+package threads.future;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableFutureDemo {
+    public static void main(String[] args) throws Exception {
+        whenComplete();
+
+    }
+
+    // 无返回值
+    public static void runAsync() throws Exception {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("run end ...");
+        });
+        future.get();
+    }
+
+
+    //有返回值
+    public static void supplyAsync() throws Exception {
+        CompletableFuture<Long> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("run end ...");
+            return System.currentTimeMillis();
+        });
+
+        long time = future.get();
+        System.out.println("time = " + time);
+    }
+
+    /**
+     * 计算结果完成时的回调方法
+     * @throws Exception
+     */
+    public static void whenComplete() throws Exception {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+            }
+            if (new Random().nextInt() % 2 >= 0) {
+                int i = 12 / 0;
+            }
+            System.out.println("run end ...");
+        });
+
+        future.whenComplete((t, action) -> System.out.println("执行完成！"));
+        future.exceptionally(t -> {
+            System.out.println("执行失败！" + t.getMessage());
+            return null;
+        });
+
+        TimeUnit.SECONDS.sleep(2);
+    }
+}
