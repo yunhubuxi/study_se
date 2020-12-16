e4e283cece977392dd0d11714bd0575bae68147c
yunhubuxi <296023290@qq.com>
2020-12-16 14:22:07 +0800

    ThreadLocal测试

---
+++ concurrency/src/main/java/threads/threadlocaltest/MyThreadLocal7.java
@@ -0,0 +1,59 @@
+package threads.threadlocaltest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MyThreadLocal7 {
+    static final InheritableThreadLocal<Map<String,Object>> threadParam = new InheritableThreadLocal<>();
+    public static void main(String[] args) {
+        ExecutorService execService1 = Executors.newFixedThreadPool(1);
+        ExecutorService execService2 = Executors.newFixedThreadPool(2);
+        execService1.execute(() -> {
+            List<String> list = new ArrayList<>();
+            list.add("a");
+            list.add("b");
+            list.add("c");
+            Map<String, Object> map = new HashMap<>();
+            map.put("test", list);
+            threadParam.set(map);
+            execService2.execute(() -> {
+                List<String> list1 = (List<String>) threadParam.get().get("test");
+                for (String s : list1) {
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(s);
+                }
+                execService2.shutdown();
+            });
+            threadParam.remove();
+            execService2.execute(() -> {
+                List<String> list1 = (List<String>) threadParam.get().get("test");
+                for (String s : list1) {
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(s);
+                }
+                execService2.shutdown();
+            });
+        });
+        execService1.shutdown();
+        System.out.println("execService1===销毁");
+
+
+
+
+
+
+    }
+}
