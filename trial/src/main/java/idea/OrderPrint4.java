ad7d191c8f0f402070a7787ba03b932d8397af0b
yunhubuxi <296023290@qq.com>
2020-12-24 11:31:44 +0800

    Create OrderPrint4.java

---
+++ trial/src/main/java/idea/OrderPrint4.java
@@ -0,0 +1,51 @@
+package idea;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class OrderPrint4 {
+    private static Semaphore A = new Semaphore(1);
+    private static Semaphore B = new Semaphore(1);
+    private static Semaphore C = new Semaphore(1);
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        B.acquire();
+        C.acquire();
+        executorService.execute(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    A.acquire();
+                    System.out.println("A");
+                    B.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        executorService.execute(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    B.acquire();
+                    System.out.println("B");
+                    C.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        executorService.execute(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    C.acquire();
+                    System.out.println("C");
+                    A.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        executorService.shutdown();
+    }
+}
