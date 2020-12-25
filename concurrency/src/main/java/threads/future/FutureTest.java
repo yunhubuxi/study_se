8b439213f0d1254904eef3861d0a0822c67c07f6
yunhubuxi <296023290@qq.com>
2020-12-25 16:47:27 +0800

    Future用法

---
+++ concurrency/src/main/java/threads/future/FutureTest.java
@@ -0,0 +1,46 @@
+package threads.future;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class FutureTest {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Callable<Long> callable = () -> {
+
+            long start = System.currentTimeMillis();
+            Thread.sleep(100);
+            long end = System.currentTimeMillis();
+
+            long seed = end - start;
+            System.out.println("seed=" + seed);
+
+            return seed;
+        };
+
+        List<Callable<Long>> tasks = new ArrayList<>();
+        tasks.add(callable);
+        tasks.add(callable);
+        tasks.add(callable);
+        tasks.add(callable);
+        tasks.add(callable);
+        tasks.add(callable);
+        tasks.add(callable);
+        tasks.add(callable);
+        tasks.add(callable);
+        tasks.add(callable);
+
+        int poolSize = Runtime.getRuntime().availableProcessors();
+        System.out.println("poolSize=" + poolSize);
+        ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
+        List<Future<Long>> futures = executorService.invokeAll(tasks);
+
+        long result = 0;
+        for (Future<Long> future : futures) {
+            result += future.get();
+        }
+        System.out.println("result=" + result);
+        executorService.shutdown();
+    }
+
+}
