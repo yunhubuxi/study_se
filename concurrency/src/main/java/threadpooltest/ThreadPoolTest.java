1a45845643895ee52a5f567b9e5ead567d1ceecd
yunhubuxi <296023290@qq.com>
2020-12-25 15:22:14 +0800

    Woker

--- concurrency/src/main/java/threadpooltest/ThreadPoolTest.java
+++ concurrency/src/main/java/threadpooltest/ThreadPoolTest.java
@@ -1,7 +1,9 @@
 package threadpooltest;
 
+import java.sql.Time;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,9 +14,39 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ThreadPoolTest {
     private static ExecutorService execService = Executors.newFixedThreadPool(1);
     public static void main(String[] args) {
-        execService.execute(() -> System.out.println("aaa"));
-        execService.execute(() -> System.out.println("bbb"));
+        Lock lock1 = new ReentrantLock();
+        Lock lock2 = new ReentrantLock();
+
+        execService.execute(() -> {
+            lock1.lock();
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("aaa");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                lock1.unlock();
+            }
+        });
+
+        execService.execute(() -> {
+            lock2.lock();
+            System.out.println("bbb");
+        });
+
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (lock1.tryLock()) {
+            System.out.println("获取lock1成功");
+        }
+        if (lock2.tryLock()) {
+            System.out.println("获取lock2失败");
+        }
     }
 
-    Lock lock = new ReentrantLock();
+
 }
