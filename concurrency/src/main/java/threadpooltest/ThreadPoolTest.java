5e5f75bb5bac33da9674864920ffdadd5ba5dcb9
yunhubuxi <296023290@qq.com>
2020-12-25 11:42:26 +0800

    线程池调试

--- concurrency/src/main/java/threadpooltest/ThreadPoolTest.java
+++ concurrency/src/main/java/threadpooltest/ThreadPoolTest.java
@@ -5,10 +5,15 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 第一个任务：创建线程
+ * 第二个任务：加入队列，第一次创建的线程会不断的从队列中获取任务去执行，只要入列成功，就自动执行了
+ */
 public class ThreadPoolTest {
-    private static ExecutorService execService = Executors.newFixedThreadPool(3);
+    private static ExecutorService execService = Executors.newFixedThreadPool(1);
     public static void main(String[] args) {
-        execService.execute(() -> System.out.println("fff"));
+        execService.execute(() -> System.out.println("aaa"));
+        execService.execute(() -> System.out.println("bbb"));
     }
 
     Lock lock = new ReentrantLock();
