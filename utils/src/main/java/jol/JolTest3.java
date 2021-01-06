ee58d45f5ae7a1214f67b5612daf60c61d551ee8
yunhubuxi <296023290@qq.com>
2021-01-06 14:05:29 +0800

    添加注释

--- utils/src/main/java/jol/JolTest3.java
+++ utils/src/main/java/jol/JolTest3.java
@@ -3,9 +3,12 @@ package jol;
 import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 import org.openjdk.jol.info.ClassLayout;
 
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * 数组是一个特殊的对象，对象头上会多4个字节存储数组长度
+ * 数组的对象大小可以通过内存布局直接算出
+ * 对象的属性如果有对象，不只基本类型的话
+ * 内存布局和对象占的内存是不一致的
+ */
 public class JolTest3 {
     public static void main(String[] args) {
         boolean[] booleans = new boolean[5];
