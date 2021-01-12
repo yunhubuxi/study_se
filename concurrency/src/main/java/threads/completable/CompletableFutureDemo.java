88acbd13724acb4bc8e791a9f5e46994df26edf8
yunhubuxi_Y400 <296023290@qq.com>
2021-01-12 23:02:01 +0800

    Update CompletableFutureDemo.java

--- concurrency/src/main/java/threads/completable/CompletableFutureDemo.java
+++ concurrency/src/main/java/threads/completable/CompletableFutureDemo.java
@@ -2,6 +2,8 @@ package threads.completable;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
@@ -16,6 +18,8 @@ import java.util.function.Supplier;
  * CompletableFuture是整合了Future和ExecutorService，并且添加了编排线程的功能
  */
 public class CompletableFutureDemo {
+    static ExecutorService executorService = Executors.newFixedThreadPool(5);
+
     public static void main(String[] args) throws Exception {
         whenComplete();
     }
@@ -51,6 +55,7 @@ public class CompletableFutureDemo {
 
     /**
      * 计算结果完成时的回调方法
+     *
      * @throws Exception
      */
     public static void whenComplete() throws Exception {
@@ -59,21 +64,28 @@ public class CompletableFutureDemo {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
             }
-            if (new Random().nextInt() % 2 >= 0) {
-                int i = 12 / 0;
-            }
+            // int a = 12 / 0;
+            int b = 1;
             System.out.println("run end ...");
+        });
+        future.thenAcceptAsync((t) -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("fff = " + t);
         });
 
         future.whenComplete((t, action) ->
-            {
-                if (action == null) {
-                    System.out.println("执行完成！");
-                } else {
-                    System.out.println("1执行失败！" + action.getMessage());
+                {
+                    if (action == null) {
+                        System.out.println("执行完成！");
+                    } else {
+                        System.out.println("1执行失败！" + action.getMessage());
+                    }
                 }
-            }
-            );
+        );
         future.exceptionally(t -> {
             try {
                 TimeUnit.SECONDS.sleep(3);
@@ -97,17 +109,18 @@ public class CompletableFutureDemo {
 
     /**
      * 当一个线程依赖另一个线程时，可以使用 thenApply 方法来把这两个线程串行化
+     *
      * @throws Exception
      */
     private static void thenApply() throws Exception {
         CompletableFuture<Long> future = CompletableFuture.supplyAsync(() -> {
             long result = new Random().nextInt(100);
-            System.out.println("result1="+result);
-            System.out.println(4/0);
+            System.out.println("result1=" + result);
+            System.out.println(4 / 0);
             return result;
         }).thenApply(t -> {
-            long result = t*5;
-            System.out.println("result2="+result);
+            long result = t * 5;
+            System.out.println("result2=" + result);
             return result;
         });
 
@@ -120,17 +133,18 @@ public class CompletableFutureDemo {
      * handle 方法和 thenApply 方法处理方式基本一样
      * 不同的是 handle 是在任务完成后再执行，还可以处理异常的任务
      * thenApply 只可以执行正常的任务，任务出现异常则不执行 thenApply 方法
+     *
      * @throws Exception
      */
-    public static void handle() throws Exception{
+    public static void handle() throws Exception {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
-            int i= 10/0;
+            int i = 10 / 0;
             return new Random().nextInt(10);
         }).handle((param, throwable) -> {
             int result = -1;
-            if(throwable==null){
+            if (throwable == null) {
                 result = param * 2;
-            }else{
+            } else {
                 System.out.println(throwable.getMessage());
             }
             return result;
@@ -140,9 +154,10 @@ public class CompletableFutureDemo {
 
     /**
      * 接收任务的处理结果，并消费处理，无返回结果
+     *
      * @throws Exception
      */
-    public static void thenAccept() throws Exception{
+    public static void thenAccept() throws Exception {
         CompletableFuture<Void> future = CompletableFuture.supplyAsync(new Supplier<Integer>() {
             @Override
             public Integer get() {
@@ -156,9 +171,10 @@ public class CompletableFutureDemo {
 
     /**
      * 跟 thenAccept 方法不一样的是，不关心任务的处理结果。只要上面的任务执行完成，就开始执行 thenAccept
+     *
      * @throws Exception
      */
-    public static void thenRun() throws Exception{
+    public static void thenRun() throws Exception {
         CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> new Random().nextInt(10)).thenRun(() -> {
             System.out.println("thenRun ...");
         });
@@ -171,7 +187,7 @@ public class CompletableFutureDemo {
     private static void thenCombine() throws Exception {
         CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "hello");
         CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> "hello");
-        CompletableFuture<String> result = future1.thenCombine(future2, (t, u) -> t+" "+u);
+        CompletableFuture<String> result = future1.thenCombine(future2, (t, u) -> t + " " + u);
         System.out.println(result.get());
     }
 
