184553d3ab4042b3494bdea65f6e62cd761d49ef
yunhubuxi <296023290@qq.com>
2021-01-18 14:42:03 +0800

    Update CompletableFutureSample5.java

--- concurrency/src/main/java/threads/completable/CompletableFutureSample5.java
+++ concurrency/src/main/java/threads/completable/CompletableFutureSample5.java
@@ -3,6 +3,8 @@ package threads.completable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,11 +12,13 @@ import java.util.concurrent.TimeUnit;
  * thenAccept不可指定线程池，会等待执行完成
  */
 public class CompletableFutureSample5 {
+    static ExecutorService executorService = Executors.newFixedThreadPool(3);
+
     public static void main(String[] args) {
         List<CompletableFuture> futures = new ArrayList();
         List<String> result = new ArrayList<>();
         CompletableFuture<String> future1 = CompletableFuture.completedFuture("test");
-        future1.thenAccept(string -> {
+        CompletableFuture<Void> future = future1.thenAccept(string -> {
             try {
                 System.out.println("===test===");
                 TimeUnit.SECONDS.sleep(6);
@@ -23,6 +27,22 @@ public class CompletableFutureSample5 {
                 e.printStackTrace();
             }
         });
+        future1.thenRunAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("===aaa===");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },executorService);
+        future.thenRunAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("===bbb===");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },executorService);
         futures.add(future1);
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
         System.out.println("运行结束");
