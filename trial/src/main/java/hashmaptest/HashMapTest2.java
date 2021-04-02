ebcb7cf95bd3303ffcc95296dc9c24c1a74bbed9
yunhubuxi <296023290@qq.com>
2021-04-02 15:22:58 +0800

    链表可以大于8，最大是11

--- trial/src/main/java/hashmaptest/HashMapTest2.java
+++ trial/src/main/java/hashmaptest/HashMapTest2.java
@@ -1,3 +1,5 @@
+package hashmaptest;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -9,20 +11,13 @@ import java.util.Objects;
  * 33 & 16 = 0
  * 49 & 16 != 0
  */
-public class HashMapTest {
+public class HashMapTest2 {
     public static void main(String[] args) {
         Map<User, String> map = new HashMap();
-        User user1 = new User(1);
-        User user2 = new User(17);
-        User user3 = new User(33);
-        User user4 = new User(49);
-        map.put(user1,"1");
-        map.put(user2,"17");
-        map.put(user3,"33");
-        map.put(user4,"49");
-        for (int i = 0; i < 10; i++) {
-            map.put(new User(i + 100), null);
+        for (int i = 0; i < 18; i++) {
+            map.put(new User(i), i + "");
         }
+
 
 
 
@@ -48,7 +43,7 @@ public class HashMapTest {
 
         @Override
         public int hashCode() {
-            return age;
+            return 1;
         }
     }
 }
